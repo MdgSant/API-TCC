132d8074810687835ca79e33d26a1c64fcb26fe5
Victor-Picciutti <victorpicciuttidossantos@gmail.com>
2026-05-29 00:17:12 -0300

    .

--- src/main/java/br/com/opala/EstudeX/entity/AtividadeConteudo.java
+++ src/main/java/br/com/opala/EstudeX/entity/AtividadeConteudo.java
@@ -1,1 +1,24 @@
- 
+package br.com.opala.EstudeX.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "TBL_ATIVIDADECONTEUDO")
+public class AtividadeConteudo
+{
+    @Id
+    @Column(name = "idAtividade")
+    private Integer idAtividade;
+
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "idAtividade")
+    private Atividade atividade;
+
+    @Column(name = "Arquivo")
+    private byte[] arquivo;
+}
